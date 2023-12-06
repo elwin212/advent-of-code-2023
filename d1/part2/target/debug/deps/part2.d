7f33fd20c4f086584fdbb042953838b65d4dfe41
elwin212 <elwin@mmisys.net>
2023-12-05 17:18:19 -0800

    remove target

--- d1/part2/target/debug/deps/part2.d
+++
@@ -1,5 +0,0 @@
-G:\Personal_Project\Rust-Proj\AdventofCode2023\d1\part2\target\debug\deps\part2.exe: src\main.rs
-
-G:\Personal_Project\Rust-Proj\AdventofCode2023\d1\part2\target\debug\deps\part2.d: src\main.rs
-
-src\main.rs:
--- d1/part2/target/debug/part2.d
+++
@@ -1,1 +0,0 @@
-G:\Personal_Project\Rust-Proj\AdventofCode2023\d1\part2\target\debug\part2.exe: G:\Personal_Project\Rust-Proj\AdventofCode2023\d1\part2\src\main.rs
